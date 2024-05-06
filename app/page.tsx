9d7c2548eb96651acd7a5c40baacdb6812027849
DIVINAETION <macbookpro2015@MACBOOKs-MacBook-Pro.local>
2024-05-06 07:09:38 +0000

    test commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'next/link';
+
+const HomePage: React.FC = () => {
+  return (
+    <div className='container max-w-none border border-white h-screen w-screen'>
+      <nav className='bg-blue-500 p-4'>
+        Logo
+      </nav>
+    </div>
+  );
+};
+
+export default HomePage;
